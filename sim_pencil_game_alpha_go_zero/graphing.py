120dd2f9cbcd34ec671e86555b72224e7611060f
matthew-gries <gries.m@husky.neu.edu>
2022-04-22 00:38:34 -0400

    more graphing

--- sim_pencil_game_alpha_go_zero/graphing.py
+++ sim_pencil_game_alpha_go_zero/graphing.py
@@ -5,7 +5,8 @@ import matplotlib.pyplot as plt
 from pathlib import Path
 import numpy as np
 
-DATA_DIRECTORY = Path(__file__).parent / "alphago_zero" / "temp"
+# DATA_DIRECTORY = Path(__file__).parent / "alphago_zero" / "temp"
+DATA_DIRECTORY = Path.home() / "Desktop" / "temp"
 
 
 def plot_win_draw_accept():
@@ -21,22 +22,22 @@ def plot_win_draw_accept():
     draws = np.load(str(draws_file))
     accepts = np.load(str(accepts_file))
 
-    fig, axes = plt.subplots(2, 1, sharex=True)
+    fig, axes = plt.subplots(3, 1, sharex=True)
 
     iteration = np.arange(0, new_wins.shape[0], step=1, dtype=int)
 
-    ax1, ax2 = axes
-    line1, = ax1.plot(iteration, new_wins / new_wins.shape[0], color="blue")
-    line2, = ax1.plot(iteration, prev_wins / prev_wins.shape[0], color="red")
-    line3, = ax1.plot(iteration, draws / draws.shape[0], color="green")
-    ax1.legend([line1, line2, line3], ["New Model Wins", "Previous Model Wins", "Draws"])
-    ax2.plot(iteration, accepts, color="tab:blue", marker="o", linestyle="None")
-    ax2.set_xlabel("Iteration")
-    ax1.set_ylabel("Percentage")
-    ax2.set_ylabel("New Model Accepted")
-    ax1.set_xticks(list(range(new_wins.shape[0])))
-    ax2.set_yticks([0, 1])
-    ax1.set_title("Win and Draw Rates")
+    total_games = new_wins[0] + prev_wins[0] + draws[0]
+    ax1, ax2, ax3 = axes
+    ax1.plot(iteration, new_wins / total_games, color="blue")
+    ax2.plot(iteration, prev_wins / total_games, color="red")
+    # line3, = ax1.plot(iteration, draws / total_games, color="green")
+    ax3.plot(iteration, accepts, color="tab:blue", marker="o", linestyle="None")
+    ax3.set_xlabel("Iteration")
+    ax1.set_ylabel("New Model Win Percentage")
+    ax2.set_ylabel("Previous Model Win Percentage")
+    ax3.set_ylabel("New Model Accepted")
+    ax3.set_yticks([0, 1])
+    ax1.set_title("Win Rates per Iteration")
 
     plt.show()
 
@@ -54,13 +55,14 @@ def plot_losses(iterations, epochs):
     pi_losses_avg = np.average(pi_losses, axis=1)
     v_losses_avg = np.average(v_losses, axis=1)
     iterations = np.arange(0, pi_losses_avg.shape[0], step=1, dtype=int)
-    fig, ax = plt.subplots()
-    line1, = ax.plot(iterations, pi_losses_avg, color="blue")
-    line2, = ax.plot(iterations, v_losses_avg, color="red")
-    ax.legend([line1, line2], ["Policy Loss", "Value Loss"])
-    ax.set_xlabel("Iteration")
-    ax.set_ylabel("Loss")
-    ax.set_title("Policy and Value Losses")
+    fig, ax = plt.subplots(2, 1, sharex=True)
+    ax1, ax2 = ax
+    ax1.plot(iterations, pi_losses_avg, color="blue")
+    ax2.plot(iterations, v_losses_avg, color="red")
+    ax2.set_xlabel("Iteration")
+    ax1.set_ylabel("Average Loss over Epochs")
+    ax2.set_ylabel("Average Loss over Epochs")
+    ax1.set_title("Policy and Value Losses")
 
     plt.show()
 
@@ -83,28 +85,26 @@ def plot_min_max_avg_loss(iterations, epochs):
     v_losses_max = np.max(v_losses, axis=1)
     iterations = np.arange(0, pi_losses_avg.shape[0], step=1, dtype=int)
 
-    fig, axes = plt.subplots(3, 1, sharex=True)
+    fig, axes = plt.subplots(2, 1, sharex=True)
 
-    ax1, ax2, ax3 = axes
+    ax1, ax2 = axes
     line11, = ax1.plot(iterations, pi_losses_min, color="blue")
-    line21, = ax1.plot(iterations, v_losses_min, color="red")
-    ax1.legend([line11, line21], ["Policy Loss", "Value Loss"])
-    ax1.set_ylabel("Minimum Loss")
-    line21, = ax2.plot(iterations, pi_losses_avg, color="blue")
+    line21, = ax1.plot(iterations, pi_losses_avg, color="red")
+    line31, = ax1.plot(iterations, pi_losses_max, color="green")
+    ax1.legend([line11, line21, line31], ["Min Loss", "Avg Loss", "Max Loss"])
+    ax1.set_ylabel("Loss")
+    ax1.set_title("Policy Loss")
+    line12, = ax2.plot(iterations, v_losses_min, color="blue")
     line22, = ax2.plot(iterations, v_losses_avg, color="red")
-    ax2.legend([line21, line22], ["Policy Loss", "Value Loss"])
-    ax2.set_ylabel("Average Loss")
-    line31, = ax3.plot(iterations, pi_losses_max, color="blue")
-    line32, = ax3.plot(iterations, v_losses_max, color="red")
-    ax3.legend([line31, line32], ["Policy Loss", "Value Loss"])
-    ax3.set_ylabel("Maximum Loss")
-    ax3.set_xlabel("Iteration")
-    ax1.set_title("Min/Avg/Max Loss Per Iteration")
+    line32, = ax2.plot(iterations, v_losses_max, color="green")
+    ax2.legend([line12, line22, line32], ["Min Loss", "Avg Loss", "Max Loss"])
+    ax2.set_ylabel("Loss")
+    ax2.set_title("Value Loss")
 
     plt.show()
 
 
 if __name__ == "__main__":
-    # plot_win_draw_accept()
-    # plot_losses(2, 10)
-    plot_min_max_avg_loss(2, 10)
+    plot_win_draw_accept()
+    # plot_losses(100, 10)
+    # plot_min_max_avg_loss(100, 10)
